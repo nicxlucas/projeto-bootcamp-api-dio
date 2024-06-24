f3aeb10a6f54225f7d44cc04697aa8f7fd424cd2
Bowie <lucasnicolauprf@gmail.com>
2024-06-24 09:00:42 -0300

    Commit inicial para versionar o Projeto DIO.me

---
+++ src/main/java/me/dio/ProjetoBootcampApiDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBootcampApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBootcampApiDioApplication.class, args);
+	}
+
+}
